9c57475ae2ae2a3d3be6763e19887fd7dbc008a6
Kernel Panic <davil544@gmail.com>
2025-06-12 22:08:12 -0400

    Switched from cin to getch to stop button presses from echoing into the console ~Added release info into the executable files as well

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <Windows.h>
+#include <conio.h>
 
 int main() {
-	// This prevents the OS from going to sleep
-	SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_AWAYMODE_REQUIRED);
+    // This prevents the OS from going to sleep
+    SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_AWAYMODE_REQUIRED);
 
-	std::cout << "Your system will now stay awake.  Press the Enter key to exit this program...";
-	std::cin.get();
+    std::cout << "Your system will now stay awake.  Press the Enter key to exit this program...";
 
-	//This returns the OS to it's previous state to allow it to go to sleep
-	SetThreadExecutionState(ES_CONTINUOUS);
-	return 0;
+    // This keeps the program running until the user presses the Enter key
+    char inputkey = 0;
+    do {
+        inputkey = _getch();
+    } while (inputkey != '\r' && inputkey != '\n');
+
+    // This returns the OS to its previous state to allow it to go to sleep
+    SetThreadExecutionState(ES_CONTINUOUS);
+    return 0;
 }
